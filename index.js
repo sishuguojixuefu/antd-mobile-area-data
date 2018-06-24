2c3e37238d4beb94d1922097a9316d7170a2e8ac
youngjuning <1003719811@qq.com>
2018-06-24 17:59:11 +0800

    正式版

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-import pc from './lib/pc-code.js'
-import pca from './lib/pca-code.js'
-import pcas from './lib/pcas-code.js'
+import pc from './lib/pc-code.json'
+import pca from './lib/pca-code.json'
+import pcas from './lib/pcas-code.json'
 
-export default {
-  pc,
-  pca,
-  pcas,
-}
+export { pc, pca, pcas }
+export default pca
