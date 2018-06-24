658990fdf5b8e974687308c8b4af939d26c379a6
youngjuning <1003719811@qq.com>
2018-06-24 18:43:53 +0800

    支持省份

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+import p from './lib/p-code.json'
 import pc from './lib/pc-code.json'
 import pca from './lib/pca-code.json'
 import pcas from './lib/pcas-code.json'
 
-export { pc, pca, pcas }
+export { p, pc, pca, pcas }
 export default pca
